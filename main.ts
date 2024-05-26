9305cc66421532f72bd9e3778b88bb9183c117d1
AlizyKhi <162714208+AlizyKhi@users.noreply.github.com>
2024-05-26 13:06:33 +0500

    Add files via upload
    Practice of UNION Literals

---
+++ main.ts
@@ -0,0 +1,39 @@
+let hours:number|string|boolean=10;
+if (hours<10) {
+    hours='0$ {hours}'
+}
+console.log(hours)
+
+let ageOfUser:number|string|boolean=100;
+if (ageOfUser<10) {
+    ageOfUser='0$ {ageOfUser}'
+}
+console.log(ageOfUser)
+
+let normalVar:string|number|boolean="Aliza";
+console.log(normalVar.toUpperCase())
+normalVar=10
+if (normalVar<10) {
+    normalVar='0$ {normalVar}'
+}
+console.log (normalVar)
+
+let myName:string|number|boolean="Musfirah";
+console.log (myName.toUpperCase())
+
+var womensCount:number|string|boolean=4;
+if (womensCount<0) {
+    womensCount='0$ {womensCount}'
+}
+console.log (womensCount)
+
+const aname:string|number|boolean="Fatima";
+console.log (aname.toUpperCase())
+
+var hairs:number|string|boolean=10000000;
+if (hairs<2) {
+    hairs='0$ {hairs}'
+}
+console.log(hairs)
+
+//
